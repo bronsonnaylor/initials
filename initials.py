79a17a61609f69f2a3d59677a87e9338f5edb1bb
bronsonnaylor <49175664+bronsonnaylor@users.noreply.github.com>
2019-04-29 04:29:14 -0600

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,4 @@
 def get_initials(fullname):
-    """ Given a person's name, returns the person's initials (uppercase) """
-    
-    #Identifies initials
     initials = ""
     names = fullname.split()
     for i in names:
@@ -9,12 +6,8 @@ def get_initials(fullname):
     caps = initials.upper()
     return caps
         
-    #Makes sure initials are capitalized.
-        
 def main():
-    print(get_initials("Oscar the grouch"))
     print(get_initials(input("What is your full name?")))
 
 if __name__ == '__main__':
     main()
-#change
